f9f42fef05c29dde3d961ab33996ff2e2b68f8a6
JulianBatHorse <julian.falck01@gmail.com>
2019-11-25 14:21:12 +0100

    fizzbuzz v1

--- src/main/java/org/example/fizzbuzz/FizzBuzz.java
+++ src/main/java/org/example/fizzbuzz/FizzBuzz.java
@@ -3,11 +3,45 @@ package org.example.fizzbuzz;
 public class FizzBuzz {
 
     public static void main(String...args) {
-    	// TODO implement me
+    	System.out.println(" ");
+        System.out.println("============================================");
+        for (int i = 1; i < 101; i++){
+
+            String fizzBuzz = fizzbuzz(i);
+            
+            if (i < 100){
+                System.out.print (fizzBuzz + ", ");
+            }else{
+                 System.out.print (fizzBuzz);
+                 System.out.println(" ");
+                 System.out.println("============================================");
+            }
+           
+
+        }
     }
 
     public static String fizzbuzz(int number) {
-    	// TODO implement me
-	    return Integer.toString(15);
+    	//Regel 5 & 3
+        if (number % 15 == 0){
+            return "FizzBuzz";
+        }
+        else
+        //Regel 3
+        if (number % 3 == 0){
+            return "Fizz";
+
+        }
+        else
+        //Regel 5
+        if (number % 5 == 0){
+            return "Buzz";
+
+        }
+        else{ 
+        //erste Regel
+        
+        return Integer.toString(number);
+        }
     }
 }
